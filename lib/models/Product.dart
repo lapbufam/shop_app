6bc79ea1a8b6bc816b6e410db7bca61c1453d784
Leonardo Augusto <lapb@icomp.ufam.edu.br>
2020-03-24 13:21:58 -0400

    Comitei o meu estado.
    Signed-off-by: Leonardo Augusto <lapb@icomp.ufam.edu.br>

---
+++ lib/models/Product.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  Product({
+    @required this.id,
+    this.title,
+    this.description,
+    this.price,
+    this.imageUrl,
+    this.isFavorite,
+  });
+}
